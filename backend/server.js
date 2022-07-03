14d0ea66363eccf8a5914668ea06a8204589a15d
makmur69 <191240000871@unisnu.ac.id>
2022-07-03 20:28:39 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
-
+app.get(`/api/goals`,(req,res)) {
+    res.send(`Get Goals`)
+}
 app.listen(port, () => console.log(`server started on port ${port}`))
