ef2274dbc6a8e4dbf3995c2d64275b75aaa92635
makmur69 <191240000871@unisnu.ac.id>
2022-07-10 23:40:34 +0700

    initial commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,9 +6,9 @@ const getGoals = (req,res) => {
 }
  // @access  Private 
 const setGoal = (req,res) => {
-    
+    res.status(400)
+    throw new Error('Please add a text field')
     if(!req.body.text){
-        res.status(400).json({message: 'Please add a text field'})
     }
 
     res.status(200).json({message: 'Set Goal'})
