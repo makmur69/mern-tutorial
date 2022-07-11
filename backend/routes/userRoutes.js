bc722cdeb5c17aa36bfc7c4e5eb3c563eaf573ae
makmur69 <191240000871@unisnu.ac.id>
2022-07-11 12:59:00 +0700

    protecting Routes & getMe()

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const {
     registerUser,
     loginUser,
-    getMe } = require('../controllers/userController.js')
+    getMe,
+  } = require("../controllers/userController.js");
+  const { protect } = require("../middleware/authMiddleware");
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
-router.get('/me', getMe)
+  router.post("/", registerUser);
+  router.post("/login", loginUser);
+  router.get("/me", protect, getMe);
 
-module.exports = router 
+module.exports = router;
