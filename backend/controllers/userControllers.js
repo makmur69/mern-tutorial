b5e4f8e1d80fdd29e6895783d0de7cbdd0b1e978
makmur69 <191240000871@unisnu.ac.id>
2022-07-11 12:47:24 +0700

    authenticate user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -54,7 +54,21 @@ const loginUser = asyncHandler(async(req,res) => {
 // @route   GET /api/users/me
 // @access  Public
 const getMe = asyncHandler(async(req,res) => {
-    res.json({ message: "User data display"})
+    const { email, password } = req.body
+
+    //check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            __id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw Error('Invalid credentials')
+    }
 })
 
 module.exports = { 
